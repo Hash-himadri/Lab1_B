61e35a112f2c717ea6efc268109c2053169d84c6
Hash-himadri <hashcodes324@gmail.com>
2023-01-05 14:17:31 +0530

    init commit

---
+++ app/src/main/java/com/himadri_parikh/lab1_b/MainActivity.java
@@ -0,0 +1,14 @@
+package com.himadri_parikh.lab1_b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
